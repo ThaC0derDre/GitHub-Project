6a5704964f6245cbd3114d801bde5945082bf336
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-19 15:40:38 -0800

    create files for reusable Buttons and Textfield

---
+++ GH Project/GFTextField.swift
@@ -0,0 +1,40 @@
+//
+//  GFTextField.swift
+//  GH Project
+//
+//  Created by Andres Gutierrez on 12/19/21.
+//
+
+import UIKit
+
+class GFTextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func configure(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        layer.borderColor = UIColor.systemGray4.cgColor
+        layer.borderWidth = 2
+        
+        textColor = .label
+        tintColor = .label
+        textAlignment = .center
+        font = UIFont.preferredFont(forTextStyle: .title2)
+        adjustsFontSizeToFitWidth = true
+        minimumFontSize = 12
+        
+        backgroundColor = .tertiarySystemBackground
+        autocorrectionType = .no
+        placeholder = "Enter a User Name"
+    }
+}
