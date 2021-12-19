da3f4d7f1e40fd832e4820d1f2709c3cf237c005
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-19 12:51:16 -0800

    create new vc's for search and favorites. create tabbar with VC's as NC

--- GH Project/FavoritesVC.swift
+++ GH Project/FavoritesVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesVC.swift
 //  GH Project
 //
 //  Created by Andres Gutierrez on 12/19/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemRed
     }
-
-
+    
 }
-
---
+++ GH Project/SearchVC.swift
@@ -0,0 +1,18 @@
+//
+//  SearchVC.swift
+//  GH Project
+//
+//  Created by Andres Gutierrez on 12/19/21.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemTeal
+    }
+    
+}
