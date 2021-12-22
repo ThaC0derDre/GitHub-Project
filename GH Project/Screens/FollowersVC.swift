6dbd840e7b11434d13e61d1f59fd0bffb6d81f2f
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-22 14:31:49 -0800

    set up collectionView in followersVC refactor VDL a bit

--- GH Project/Screens/FollowersVC.swift
+++ GH Project/Screens/FollowersVC.swift
@@ -10,12 +10,33 @@ import UIKit
 class FollowersVC: UIViewController {
     
     var userName: String!
+    var collectionView: UICollectionView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        getFollowers()
+        configureCollectionView()
+       
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: true)
+    }
+    
+    func configureCollectionView(){
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewFlowLayout())
+        view.addSubview(collectionView)
+        collectionView.backgroundColor = .systemCyan
+        collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func configureViewController(){
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
-        
+    }
+    
+    func getFollowers(){
         NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
            
             switch result{
@@ -30,9 +51,5 @@ class FollowersVC: UIViewController {
               }
             }
         }
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        navigationController?.setNavigationBarHidden(false, animated: true)
     }
 }
