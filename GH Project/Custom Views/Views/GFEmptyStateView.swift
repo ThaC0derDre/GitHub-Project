d82f662dfc47152ad10cc2706f0b2bda0358c755
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-27 07:09:54 -0800

    add group folders for refactor

---
+++ GH Project/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,23 @@
+//
+//  GFEmptyStateView.swift
+//  GH Project
+//
+//  Created by Andres Gutierrez on 12/27/21.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+   private func configure(){
+        
+    }
+}
