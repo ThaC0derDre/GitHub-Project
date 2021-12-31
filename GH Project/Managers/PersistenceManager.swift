0dd7732caaa9465b5dfed0bba64bdc813add45b2
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-31 14:51:09 -0800

    create enum for add or remove case. add error message if follower is already added create func to update (add, delete ) UserDefault array

--- GH Project/Managers/PersistenceManager.swift
+++ GH Project/Managers/PersistenceManager.swift
@@ -7,6 +7,10 @@
 
 import Foundation
 
+enum PersistenceActionType {
+    case add, remove
+}
+
 enum PersistenceManager {
     
     static private let defaults = UserDefaults.standard
@@ -14,6 +18,33 @@ enum PersistenceManager {
     enum Keys {
         static let favorites = "favorites"
     }
+    
+    static func updateWith(favorite:Followers, actionType: PersistenceActionType, completed: @escaping (GFError?) -> Void){
+        retrieveFavorites { result in
+            switch result{
+            case .success(let favorites):
+                var retrievedFavorites = favorites
+                
+                switch actionType {
+                case .add:
+                    guard !retrievedFavorites.contains(favorite) else{
+                        completed(.alreadyFavorited)
+                        return
+                    }
+                    retrievedFavorites.append(favorite)
+                    
+                case .remove:
+                    retrievedFavorites.removeAll { $0.login == favorite.login }
+                }
+                
+                completed(save(favorites: retrievedFavorites))
+                          
+            case .failure(let error):
+                completed(error)
+            }
+        }
+    }
+    
     
     static func retrieveFavorites(completed: @escaping (Result<[Followers],GFError>) -> Void){
         guard let favoritesData = defaults.object(forKey: Keys.favorites) as? Data else{
--- GH Project/Utilites/GFError.swift
+++ GH Project/Utilites/GFError.swift
@@ -8,9 +8,10 @@
 import Foundation
 
 enum GFError: String, Error{
-    case invalidUsername     = "This username create an invalid request. Please try again."
-    case unableToComplete    = "Unable to complete your request. Please check your internet connection."
-    case invalidResponse     = "Invalid response from the server. Please try again."
-    case invalidData         = "The data received from the server was invalid. Please try again."
-    case unableToFavorite    = "Unable to favorite this user. Please try again."
+    case invalidUsername        = "This username create an invalid request. Please try again."
+    case unableToComplete       = "Unable to complete your request. Please check your internet connection."
+    case invalidResponse        = "Invalid response from the server. Please try again."
+    case invalidData            = "The data received from the server was invalid. Please try again."
+    case unableToFavorite       = "Unable to favorite this user. Please try again."
+    case alreadyFavorited       = "This person is already in your Favorites"
 }
