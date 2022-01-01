69412a0455085bd60c47f703f88f1ce3c26c7e9d
AndresGutierrez <c0dingw.dre@gmail.com>
2021-12-31 22:26:01 -0800

    configure tableView and ViewController. add delegate and datasource methods

--- GH Project/Screens/FavoritesVC.swift
+++ GH Project/Screens/FavoritesVC.swift
@@ -9,10 +9,43 @@ import UIKit
 
 class FavoritesVC: UIViewController {
 
+    let tableView               = UITableView()
+    var favorites:[Followers]   = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemBackground
+        configureViewController()
+        configureTableView()
     }
     
+    func configureViewController(){
+        
+        title                       = "Favorites"
+        tableView.backgroundColor   = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    func configureTableView(){
+        
+        view.addSubview(tableView)
+        tableView.frame      = view.bounds
+        tableView.rowHeight  = 80
+        tableView.delegate   = self
+        tableView.dataSource = self
+        tableView.register(FavoritesCell.self, forCellReuseIdentifier: FavoritesCell.reuseID)
+    }
+}
+extension FavoritesVC: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favorites.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: FavoritesCell.reuseID) as! FavoritesCell
+        let favorite = favorites[indexPath.row]
+        cell.set(favorites: favorite)
+        return cell
+    }
 }
