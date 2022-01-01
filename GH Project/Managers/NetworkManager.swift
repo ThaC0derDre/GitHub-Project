c58af246b5d046c71f0a77fd008addf49224dfb4
AndresGutierrez <c0dingw.dre@gmail.com>
2022-01-01 10:36:08 -0800

    delete string ext that was a date formatter. in place of that, in Network Manager, add decoding strategy to format for us

--- GH Project/Managers/NetworkManager.swift
+++ GH Project/Managers/NetworkManager.swift
@@ -72,8 +72,9 @@ class NetworkManager {
             }
             // All good? Lets decode the data
             do{
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                let decoder                  = JSONDecoder()
+                decoder.keyDecodingStrategy  = .convertFromSnakeCase
+                decoder.dateDecodingStrategy = .iso8601
                 let user = try decoder.decode(User.self, from: data)
                 completed(.success(user))
             }catch{
--- GH Project/Screens/UserInfoVC.swift
+++ GH Project/Screens/UserInfoVC.swift
@@ -62,7 +62,7 @@ class UserInfoVC: UIViewController {
         self.add(childVC: GFUserHeaderInfoVC(user: user), to: self.headerView)
         self.add(childVC: repoItemVC, to: self.itemView1)
         self.add(childVC: followerItemVC, to: self.itemView2)
-        self.dateLabel.text = "GitHub since \(user.createdAt.convertToDisplayFormat())"
+        self.dateLabel.text = "GitHub since \(user.createdAt.convertToMonthYearFormat())"
     }
     
     func layoutUI(){
