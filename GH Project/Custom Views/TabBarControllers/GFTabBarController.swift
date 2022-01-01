b797480790bb30e9a3a114931005c2ad79a0f077
AndresGutierrez <c0dingw.dre@gmail.com>
2022-01-01 09:04:43 -0800

    optimization clean up SceneDelegate

---
+++ GH Project/Custom Views/TabBarControllers/GFTabBarController.swift
@@ -0,0 +1,41 @@
+//
+//  GFTabBarController.swift
+//  GH Project
+//
+//  Created by Andres Gutierrez on 1/1/22.
+//
+
+import UIKit
+
+class GFTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().tintColor = .systemBlue
+        self.viewControllers            = [searchNC(), favoritesNC()]
+        
+    }
+ 
+    func searchNC() -> UINavigationController {
+        let searchNC = SearchVC()
+        searchNC.title = "Search"
+        searchNC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchNC)
+    }
+    
+    func favoritesNC() -> UINavigationController {
+        let favoritesNC = FavoritesVC()
+        favoritesNC.title = "Favorites"
+        favoritesNC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesNC)
+    }
+    
+    
+    // Do we want to change the NavBar appperance?
+//    func configureTabBar(){
+//        UINavigationBar.appearance().tintColor =
+//    }
+}
--- GH Project/Supporting Files/SceneDelegate.swift
+++ GH Project/Supporting Files/SceneDelegate.swift
@@ -17,67 +17,20 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
-        window?.windowScene = windowScene
-        window?.rootViewController = createTabBar()
+        window?.windowScene         = windowScene
+        window?.rootViewController  = GFTabBarController()
         window?.makeKeyAndVisible()
     }
     
-    func searchNC() -> UINavigationController {
-        let searchNC = SearchVC()
-        searchNC.title = "Search"
-        searchNC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
-        
-        return UINavigationController(rootViewController: searchNC)
-    }
-    
-    func favoritesNC() -> UINavigationController {
-        let favoritesNC = FavoritesVC()
-        favoritesNC.title = "Favorites"
-        favoritesNC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
-        
-        return UINavigationController(rootViewController: favoritesNC)
-    }
-    
-    func createTabBar() -> UITabBarController {
-        let tabbar = UITabBarController()
-        UITabBar.appearance().tintColor = .systemBlue
-        tabbar.viewControllers = [searchNC(), favoritesNC()]
-        
-        return tabbar
-    }
-    // Do we want to change the NavBar appperance?
-//    func configureTabBar(){
-//        UINavigationBar.appearance().tintColor =
-//    }
-    
+    func sceneDidDisconnect(_ scene: UIScene) { }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
-    }
+    func sceneDidBecomeActive(_ scene: UIScene) { }
 
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
+    func sceneWillResignActive(_ scene: UIScene) { }
 
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
+    func sceneWillEnterForeground(_ scene: UIScene) { }
 
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
+    func sceneDidEnterBackground(_ scene: UIScene) { }
 
 
 }
